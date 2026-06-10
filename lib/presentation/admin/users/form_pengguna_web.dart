1333ae1c1becbaa11c92f156f4afeebea9c913e0
justChandra27 <muchpr271@gmail.com>
2026-06-10 10:10:50 +0800

    form pengguna web dart fix

--- lib/presentation/admin/users/form_pengguna_web.dart
+++ lib/presentation/admin/users/form_pengguna_web.dart
@@ -391,13 +391,8 @@ class _FormPenggunaWebState extends State<FormPenggunaWeb> {
         children: [
           CircleAvatar(
             radius: 24,
-<<<<<<< HEAD
-            backgroundColor: color.withOpacity(.15),
-            child: Icon(icon, color: color, size: 24),
-=======
             backgroundColor: color.withValues(alpha: .15),
             child: Icon(icon, color: color),
->>>>>>> eabe87012266b1c56deb36bb6f629a0c2fc752c7
           ),
 
           const SizedBox(width: 16),
