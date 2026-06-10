745d80f0b250d7a13657fcffac3841afcd22838b
justChandra27 <muchpr271@gmail.com>
2026-06-10 10:07:38 +0800

    testing kategori

--- lib/presentation/admin/users/form_pengguna_web.dart
+++ lib/presentation/admin/users/form_pengguna_web.dart
@@ -436,12 +436,8 @@ class _FormPenggunaWebState extends State<FormPenggunaWeb> {
           Row(
             children: [
               CircleAvatar(
-<<<<<<< HEAD
-                backgroundColor: Colors.blue.withOpacity(.2),
-=======
                 backgroundColor:
                     Colors.blue.withValues(alpha: .2),
->>>>>>> eabe87012266b1c56deb36bb6f629a0c2fc752c7
                 child: Text(
                   user.name.isNotEmpty
                       ? user.name
